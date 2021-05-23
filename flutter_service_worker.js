8047a5f7fa7df423d7ff23682a7eb2331c8e5323
Van Wyk <vanwyk.pretorius@ahghealth.co.za>
2021-05-23 15:05:10 +0200

    Medications and login

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -14,8 +14,8 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "0ee7b2d8f0f448ed6a00f7deb34aa850",
-"/": "0ee7b2d8f0f448ed6a00f7deb34aa850",
+"index.html": "34caf93fb6382a3a89cd2decb86b92cf",
+"/": "34caf93fb6382a3a89cd2decb86b92cf",
 "main.dart.js": "2de8150497e8a9c20b73638e9dcf04fa",
 "manifest.json": "3d46a8d11d4beeeceb1e1b65d28f3e97",
 "version.json": "0cdb396baf0c159e218abe5cb7e92600"
